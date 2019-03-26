723e655e0d14a92c3887f4330c66edca10d873ca
garlam <garlam_au@qq.com>
2019-03-27 01:15:24 +0800

    update

--- src/main/java/com/SpringbootApplication.java
+++ src/main/java/com/SpringbootApplication.java
@@ -4,9 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+/*@ComponentScan(basePackages= {"com"})
+* scan project component which not under the SpringbootApplication.class
+* */
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
